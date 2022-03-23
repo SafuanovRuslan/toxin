7932426ecb6ad260566569646f9e55482e68c4b8
Safuanov Ruslan <safuanov.ru@yandex.ru>
2022-03-23 20:54:15 +0300

    Day 1

--- viewes/header.php
+++ viewes/header.php
@@ -1,5 +1,31 @@
 <header>
-    <div class="header__wrapper">
-        
+    <div class="centrator">
+        <div class="header__wrapper">
+            <div class="logo">
+                <a href="/"><img src="img/header/logo.svg" alt="toxin"></a>
+            </div>
+            <nav class="header-nav">
+                <ul class="nav nav-tabs header-nav__list">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Active</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+
+                <button class="button_white">Войти</button>
+                <button class="button_purple">Зарегистрироваться</button>
+            </nav>
+        </div>
     </div>
 </header>
